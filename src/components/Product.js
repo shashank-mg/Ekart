f803865d6c77bc4c9102025bd8d99a1a0bb26f8c
shashank-mg <52480486+shashank-mg@users.noreply.github.com>
2019-09-04 11:34:10 +0530

    Update Product.js
    new update

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,36 +2,94 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {ProductConsumer} from './Context';
+import PropTypes from 'prop-types'; //To prevent data from being modified
 
 export default class Product extends Component {
     render() {
         const {id,title,img,price,inCart}=this.props.product;
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                <div className="card"> {/* bootstrap class 'card' */}
+                <div className="card"> {/* bootstrap class */}
                     <div className="img-container p-5" onClick={console.log("Clicked")}>
                         <Link to="/details">
                             <img src={img} alt="product" className="card-img-top"></img>
                         </Link>
-                        <button style={{height:'20px',width:'20px'}} className="card-btn" disabled={inCart?true:false} onClick={()=>{console.log('added to the cart')}}>                            
-                            {inCart?(<p className="text-capitalize mb-0" disabled>{" "}is inCart</p>):(<i className="fas fa-cart-plus"/>)}
+                        <button  className="btn btn-outline-default" disabled="disabled">                            
+                            <img  src="https://cdn0.iconfinder.com/data/icons/shopping-icons-5/100/Cart-512.png" width="20px" height="20px" alt="Add to Cart"/>
+                            Add to Cart
                         </button>
+                       {/* <button style={{height:'20px',width:'20px'}} className="card-btn" disabled={inCart?true:false} onClick={()=>{console.log('added to the cart')}}>                            
+                            {inCart?(<p className="text-capitalize mb-0" disabled>{" "}is inCart</p>):(<i className="fas fa-cart-plus"/>)}
+                           </button>
+                       */}
                     </div>
                 </div>            
                 {/* Card footer */}
-                <div className="card-footer d-flex justify-content-between"> {/* bootstrap class footer */}
+                <div className="card-footer d-flex justify-content-between">
                 <p className="align-self-center mb-0" style={{fontFamily:'Trebuchet MS'}}>{title}</p>
-                <p className="align-self-center mb-0" style={{diplay:'flex',fontFamily:'Trebuchet MS'}}>{price} $</p>
+                <h5 className="align-self-center mb-0" style={{display:'flex',fontFamily:'Trebuchet MS',fontStyle:'italic'}}>$ {price}</h5>
                 </div>     
             </ProductWrapper>
         )
     }
 }
-const ProductWrapper = styled.div``
 
-/*
- 1. Remember to always give the link i.e <Link> in the file where the single or indivisual data is handled (Product.js), and not in the file 
-    where all the data is handled i.e in arrays. (ProductList.js) 
- 2. Data from ProductList to Product is coming from props, but data from Context.js to ProductList.js is coming from ContextAPI. 
-*/
+//Dont touch.
+Product.propTypes = {
+    product:PropTypes.shape({
+        id:PropTypes.number,
+        img:PropTypes.string,
+        title:PropTypes.string,
+        price:PropTypes.number,
+        inCart:PropTypes.bool
+    }).isRequired
+}
+
+const ProductWrapper = styled.div`
+            .card{                
+                transition:all 0.1s linear;                  
+            }
+            .card-footer{
+                background:transparent;
+                border-top:transparent;
+                transition:all 0.1s linear;
+            }
+            &:hover{
+                .card{
+                    border:0.1rem solid purple;
+                    box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+                }
+                .card-footer{
+                    background:rgba(247,247,247);                   
+                }
+            }        
+           .img-container{
+               postion:relative;
+               overflow:hidden;
+           }
+           &:hover .card-img-top{
+               transform:scale(1.06);
+           }     
+           .card-img-top{
+               transition: all 0.1s linaer;
+           }
+           .btn{
+               position:absolute;
+               bottom:0;
+               right:0;
+               padding:0.4rem 0.6rem;
+               border:none;
+               background-color:mediumpurple;
+               border-radius: 0.5 0 0 0;              
+           } 
+           &:hover{
+               transform:scale(1.05);
+           }
+
+`;
+
+// transition-- something happens after a specified time interval, 'all' means transition property should be applicalble to all properties. and is of the type 'linear'.
+// overflow is used if the image is bigger and the extra contents are to be hidden
+// transform is used for animation , scale is used here to increase the size of the image when hovered.
