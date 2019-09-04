0bcea14c93436dee0d9818b6c830448f6cea5caa
shashank-mg <52480486+shashank-mg@users.noreply.github.com>
2019-09-04 11:35:25 +0530

    Update Context.js

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import {storeProducts,detailProduct} from './Data';
 
+
 const ProductContext = React.createContext(); 
 
 //Place the ProductContext.Provider on top of the entire application.i.e in index.js
 //Do the same thing as done in ProductList i.e getting data from the Data.js and put it in an array.
 class ProductProvider extends Component {
     state={
-        products : storeProducts,
-        detaiProduct : detailProduct
+        products:storeProducts,  //assigning the objects to the variables, means passing them as a reference, storeProducts is the array of objects
+        detaiProduct:detailProduct
     }
     handleDetail = () =>{ // To open up the details page
         console.log("hello from detail");
@@ -16,6 +17,10 @@ class ProductProvider extends Component {
     }
     addToCart = () =>{ // To add the item to the cart
         console.log("hello from addToCart");
+    }
+    tester = () => {
+        console.log('State products :',this.state.products[0].inCart);
+        console.log('Data products :',storeProducts[0].inCart);
     }
     render() {
         return (
@@ -29,4 +34,5 @@ class ProductProvider extends Component {
 
 //Consumer
  const ProductConsumer = ProductContext.Consumer;
+
  export {ProductProvider,ProductConsumer};
